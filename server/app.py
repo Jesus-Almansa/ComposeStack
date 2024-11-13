c7358281b95c9de95212e4045e6af3425fabdecd
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-13 20:40:38 +0100

    renderizar el endpoint de la imagen

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,8 +7,11 @@ app = Flask(__name__)
 def index():
     return {
         "channel": "Hello, World!",
-        "tutorial":"React, Flask & Docker"
+        "tutorial":"React, Flask & Docker hijo de la gran"
     }
+@app.route("/api/naruto", methods=["GET"])
+def naruto():
+    return render_template("naruto.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
